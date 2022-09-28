39d0895721f057ddf70208f34a01e7896c6d76b7
sinapy <sinasarsharpour@cmail.carleton.ca>
2022-09-27 20:27:04 -0400

    Week04

--- server.js
+++ server.js
@@ -14,28 +14,30 @@
 // console.log("Server running at http://localhost:3000/")
 
 // Connect 1
-const connect = require ('connect');
-const app = connect();
-app.listen(3000);
 
-function logger(req, res, next){
-    console.log(req.method, req.url);
+// const app = connect();
+// app.listen(4200);
+//
+// function logger(req, res, next){
+//     console.log(req.method, req.url);
+//
+//     next();
+// }
+//
+// function helloWorld(req, res, next) {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// };
+//
+// function goodbye(req, res, next) {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Good bye');
+// };
+//
+// app.use(logger);
+// app.use('/hello',helloWorld);
+// app.use('/goodbye',goodbye);
+//
+// console.log('Server running at http://localhost:4200/')
 
-    next();
-}
 
-function helloWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-};
-
-function goodbye(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Good bye');
-};
-
-app.use(logger);
-app.use('/hello',helloWorld);
-app.use('/goodbye',goodbye);
-
-console.log('Server running at http://localhost:3000/')
